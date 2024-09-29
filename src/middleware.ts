518a560f41877fc1bf43c2070c5af34a15a0a484
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-09-29 08:15:22 +0700

    Refactor middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,11 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export default function middleware(req: NextRequest) {
-  const isLogin = false;
+  const isLogin = true;
   if (!isLogin) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
-  return NextResponse.redirect(new URL('/', req.url));
+  return NextResponse.next();
 }
 
 export const config = {
